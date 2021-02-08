5a1de707aea03baf8ee5cf9394885a164254582d
Pragya Tiwary <pragyatiwari101@gmail.com>
2021-02-08 16:16:54 +0530

    I have changed the logic

--- LearningGIT.java
+++ LearningGIT.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class LearningGIT {
 	
 	private static void gitMethod(int number){
-		if(number>0) {
-			for(int iter=0; iter<number; iter++) {
+		if(number/2==0) {
+			for(int iter=0; iter<number; iter+=2) {
 				System.out.print("Learning phase: " + iter + "\t");
 				System.out.println("Learning phase: " + iter + "\tComplete");
 			}
 		}
+		else
+			System.out.println("\n\tWe accept only even numbers.");
 	}
 
 	public static void main(String[] args) {
